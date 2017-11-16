a04467dc23dc143380fc1396e1188c2bf5953a31
Alexander Poschenreithner <alexander.poschenreithner@gmail.com>
2017-11-16 19:17:03 +0100

    Bugfix of some paths; Remove tar.gz if it already exists

--- infrastructure/attacker/share/attack-scripts/05_netcat_file_transfere.sh
+++ infrastructure/attacker/share/attack-scripts/05_netcat_file_transfere.sh
@@ -7,10 +7,15 @@
 
 # https://nakkaya.com/2009/04/15/using-netcat-for-file-transfers/
 
-rm badcode.tgz
+# Delete badcode.tgz if it already exists
+if [ -f '/vagrant_data/badcode.tgz' ]
+then
+	echo "     => Removing old badcode.tgz"
+	rm /vagrant_data/badcode.tgz
+fi
 
 echo "     => Creating tgz from /share/netcat_file_content..."
-env GZIP=-9 tar cvzf badcode.tgz -C /vagrant_data/netcat_file_content/ .
+env GZIP=-9 tar cvzf /vagrant_data/badcode.tgz -C /vagrant_data/netcat_file_content/ .
 echo "     => Sending file over netcat on port 1234 to Server"
-nc -w 3 10.11.12.10 1234 < badcode.tgz
+nc -w 3 10.11.12.10 1234 < /vagrant_data/badcode.tgz
 echo "     => Sent!"
